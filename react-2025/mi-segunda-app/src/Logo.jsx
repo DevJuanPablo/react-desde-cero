b49aa6ca68eff7101e83f3b7b1aa974adb5f5d51
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-12 21:42:40 -0400

    se añade el archivo Logo.jsx al repositorio

---
+++ react-2025/mi-segunda-app/src/Logo.jsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div>
+      <img src="https://edteam-media.s3.amazonaws.com/app/utils/logo/isotipo-color.svg" alt="Logo de la empres" />
+    </div>
+  )
+}
+
+export default Logo
