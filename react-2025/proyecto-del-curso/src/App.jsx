d1ec4caa92575416f5da1f1ef88a07048e54f11a
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-13 19:19:40 -0400

    implementamos un fetch (api publica) y utilizamos useState, useEffect

--- react-2025/proyecto-del-curso/src/App.jsx
+++ react-2025/proyecto-del-curso/src/App.jsx
@@ -1,30 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const API_KEY = '21a08aa94f78667f28d58cb2c41cb7413eef3db9d6b5bff0e2be8eba03bc6dce'
+
+  const [criptos, setCriptos] = useState()
+  
+  useEffect(() => {
+    fetch(`https://rest.coincap.io/v3/assets?limit=100`,
+      {headers: {Authorization: `Bearer ${API_KEY}`}}
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCriptos(data.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)  
+      })
+  }, [])
+
+  if (!criptos)
+    return <span>Cargando...</span>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        { criptos.map( ({name, priceUsd}) => (
+          <li>Nombre: { name } Precio: {priceUsd}</li>
+        ))}
+      </ol>
     </>
   )
 }
